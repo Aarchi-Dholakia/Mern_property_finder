ca5b8ca4fcb2b7ca8abfcb9a2aa1f6c6ca9680d2
Aarchi-Dholakia <dholakiaak@gmail.com>
2025-06-10 21:13:58 +0530

    created api routes for viewing user's listings

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { deleteUser, test } from '../controllers/user.controllers.js';
+import { deleteUser, getUserListings, test } from '../controllers/user.controllers.js';
 // import { deleteUser, test, updateUser, getUserListings, getUser } from '../controllers/user.controller.js';
 import { updateUser } from '../controllers/user.controllers.js';
 import { verifyToken } from '../utils/verifyUser.js';
@@ -10,7 +10,7 @@ const router = express.Router();
 router.get('/test', test);
 router.post('/update/:id', verifyToken, updateUser)
 router.delete('/delete/:id', verifyToken, deleteUser)
-// router.get('/listings/:id', verifyToken, getUserListings)
+router.get('/listings/:id', verifyToken, getUserListings)
 // router.get('/:id', verifyToken, getUser)
 
 export default router;
